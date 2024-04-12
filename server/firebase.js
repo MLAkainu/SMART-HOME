52b5ecede8b5912a9677d223e799ef29c27c7dab
khanhtoan <toan.trancs12462@hcmut.edu.vn>
2024-04-12 19:41:43 +0700

    update api

--- server/firebase.js
+++ server/firebase.js
@@ -11,6 +11,8 @@ import {
   getDoc,
   serverTimestamp,
   getDocs,
+  deleteDoc,
+  updateDoc,
 } from "firebase/firestore";
 import { Router } from "express";
 import { firebaseConfig } from "./config.js";
@@ -37,27 +39,19 @@ export const initializeFirebaseApp = () => {
 
 const createUser = async (req, res) => {
   try {
-    const userName = req.body.userName;
-    delete req.body.userName;
-    const usersRef = doc(db, "Users",userName);
-    // const q = query(usersRef, where("userName", "==", req.body.userName));
-    // const user = await getDocs(q);
-    //console.log(user);
-    const user = await getDoc(usersRef);
-    // const docs=[]
-    // user.forEach((doc) => {
-    //   const info = doc.data();
-    //   delete info.password;
-    //   docs.push(info);
-    // });
-    // if (docs.length > 0) {
-    //   return res.status(400).json({msg:'username already exist'})
-    // }
-    if (user.exists()) {
-      return res.status(400).json({ msg: "username already exist" });
+    const userRef = collection(db, 'Users');
+    const q = query(userRef, where("userName", '==', req.body.userName));
+    let userId;
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      userId=doc.id;
+    });
+    if (userId){
+      res.status(400).json({msg:"username already exist"})
+    } else {
+      await addDoc(userRef, req.body)
+      res.status(200).json({msg:'user created'})
     }
-    await setDoc(usersRef, req.body);
-    res.status(201).json({ msg: "user created" });
   } catch (err) {
     console.log(err);
   }
@@ -65,41 +59,97 @@ const createUser = async (req, res) => {
 
 const getUserByUserName = async (req, res) => {
   try {
-    const userName = req.body.userName;
-    delete req.body.userName;
-    const usersRef = doc(db, "Users", userName);
-    const user = await getDoc(usersRef);
-    if (user.exists()) {
-      const userData = user.data()
-      delete userData.password;
-      return res.status(200).json(userData)
+    const userRef = collection(db, "Users");
+    const q = query(userRef, where("userName", "==", req.params.userName));
+    let user;
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      user = doc.data();
+    });
+    if (user) {
+      delete user.password;
+      res.status(200).json(user);
+    } else {
+      res.status(404).json({ msg: "user not found" });
     }
-    return res.status(404).json({msg:'not found'})
+  } catch (err) {
+    console.log(err);
   }
-  catch (err) {
-    console.log(err)
-  }
-
 };
+
+const updateUser = async (req, res) => {
+  try {
+    const userRef = collection(db, "Users");
+    const q = query(userRef, where("userName", "==", req.params.userName));
+    let userId;
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      userId = doc.id;
+    });
+    if (userId) {
+      await updateDoc(doc(db, "Users", userId),req.body);
+      res.status(200).json({msg:"user updated"});
+    } else {
+      res.status(404).json({ msg: "user not found" });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+const deleteUser = async (req, res) => {
+  try {
+    const userRef = collection(db, "Users");
+    const q = query(userRef, where("userName", "==", req.params.userName));
+    let userId;
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      userId = doc.id;
+    });
+    if (userId) {
+      const notifQuerySnapShot = await getDocs(
+        collection(db, "Users", userId, "Notifs")
+      );
+      notifQuerySnapShot.forEach(async (document) => {
+        await deleteDoc(doc(db,'Users',userId,'Notifs',document.id))
+      });
+      const actQuerySnapShot = await getDocs(
+        collection(db, "Users", userId, "Activities")
+      );
+      actQuerySnapShot.forEach(async (document) => {
+        await deleteDoc(doc(db, "Users", userId, "Activities", document.id));
+      });
+      await deleteDoc(doc(db, 'Users', userId));
+      res.status(200).json({ msg: "user deleted" });
+    } else {
+      res.status(404).json({ msg: "user not found" });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
 
 const createNotif = async (req, res) => {
   try {
-    const userName = req.body.userName;
-    delete req.body.userName;
-    const usersRef = doc(db, "Users", userName);
-    const user = await getDoc(usersRef);
-    if (user.exists()) {
-      const notifRef = collection(db, "Users", userName, "Notifs");
+    const userRef = collection(db, "Users");
+    const q = query(userRef, where("userName", "==", req.params.userName));
+    let userId;
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      userId = doc.id;
+    });
+    if (userId) {
+      const notifRef = collection(db, "Users", userId, "Notifs");
       const notif = {
         type: req.body.type,
         content: req.body.content,
-        createdAt:serverTimestamp(),
+        createdAt: serverTimestamp(),
       };
       await addDoc(notifRef, notif);
       res.status(200).json({ msg: "notif created" });
     }
     else {
-      res.status(404).json({msg:"user not found"})
+      res.status(404).json({ msg: "user not found" });
     }
   }
   catch (err) {
@@ -107,24 +157,29 @@ const createNotif = async (req, res) => {
   }
 }
 
-const getNotifsByUserName = async (req, res) => {
+const getNotifs = async (req, res) => {
   try {
-    const userName = req.body.userName;
-    delete req.body.userName;
-    const usersRef = doc(db, "Users", userName);
-    const user = await getDoc(usersRef);
-    if (user.exists()) {
-      const querySnapShot = await getDocs(collection(db, "Users", userName, "Notifs"))
-      const notifs = []
-      querySnapShot.forEach(doc => {
+    const userRef = collection(db, "Users");
+    const q = query(userRef, where("userName", "==", req.params.userName));
+    let userId;
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      userId = doc.id;
+    });
+    if (userId) {
+      const querySnapShot = await getDocs(
+        collection(db, "Users", userId, "Notifs")
+      );
+      const notifs = [];
+      querySnapShot.forEach((doc) => {
         let data = doc.data();
-        const time = new Date(data.createdAt.toDate())
+        const time = new Date(data.createdAt.toDate());
         data = {
           ...data,
-          createdAt: time
-        }
-        notifs.push(data)
-      })
+          createdAt: time,
+        };
+        notifs.push(data);
+      });
       res.status(200).json(notifs);
     } else {
       res.status(404).json({ msg: "user not found" });
@@ -134,8 +189,67 @@ const getNotifsByUserName = async (req, res) => {
   }
 }
 
+const createActivity = async (req, res) => {
+try {
+  const userRef = collection(db, "Users");
+  const q = query(userRef, where("userName", "==", req.params.userName));
+  let userId;
+  const querySnapshot = await getDocs(q);
+  querySnapshot.forEach((doc) => {
+    userId = doc.id;
+  });
+  if (userId) {
+    const actRef = collection(db, "Users", userId, "Activitites");
+    const act = {
+      type: req.body.type,
+      content: req.body.content,
+      createdAt: serverTimestamp(),
+    };
+    await addDoc(actRef, act);
+    res.status(200).json({ msg: "notif created" });
+  } else {
+    res.status(404).json({ msg: "user not found" });
+  }
+} catch (err) {
+  console.log(err);
+}
+}
 
-router.route("/users").post(createUser).get(getUserByUserName);
-router.route("/notifs").post(createNotif).get(getNotifsByUserName);
+const getActivities = async (req, res) => {
+  try {
+    const userRef = collection(db, "Users");
+    const q = query(userRef, where("userName", "==", req.params.userName));
+    let userId;
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      userId = doc.id;
+    });
+    if (userId) {
+      const querySnapShot = await getDocs(
+        collection(db, "Users", userId, "Activities")
+      );
+      const acts = [];
+      querySnapShot.forEach((doc) => {
+        let data = doc.data();
+        const time = new Date(data.createdAt.toDate());
+        data = {
+          ...data,
+          createdAt: time,
+        };
+        acts.push(data);
+      });
+      res.status(200).json(atcs);
+    } else {
+      res.status(404).json({ msg: "user not found" });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+router.route("/users").post(createUser);
+router.route("/:userName").get(getUserByUserName).put(updateUser).delete(deleteUser);
+router.route("/:userName/notifs").post(createNotif).get(getNotifs);
+router.route('/:userName/activities').get(getActivities).post(createActivity);
 
 export default router;
